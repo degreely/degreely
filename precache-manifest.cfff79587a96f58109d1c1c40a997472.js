c0503829ac6143238ed2d4c033ee94d2ba8b3200
Dominique Ng <dominique@u.nus.edu>
2020-10-14 18:03:26 +0800

    Updates

---
+++ precache-manifest.cfff79587a96f58109d1c1c40a997472.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e3270a54ba0503a1fb767be724115a",
+    "url": "/degreely/index.html"
+  },
+  {
+    "revision": "3fc7bf37030ed5b93ba0",
+    "url": "/degreely/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "808bd41fb8d66850829a",
+    "url": "/degreely/static/js/2.540f7985.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/degreely/static/js/2.540f7985.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fc7bf37030ed5b93ba0",
+    "url": "/degreely/static/js/main.9c4c7eb6.chunk.js"
+  },
+  {
+    "revision": "e89e318310450ef21868",
+    "url": "/degreely/static/js/runtime-main.8d5e98b3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/degreely/static/media/logo.5d5d9eef.svg"
+  }
+]);
