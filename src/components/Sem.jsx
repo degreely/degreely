c819c017d4c74a8ccd7c1b86e7cf72ecdc210268
Kenneth Fung <fungchenyu@rocketmail.com>
2020-10-16 17:50:04 +0800

    Move sem name out of droppable area

--- src/components/Sem.jsx
+++ src/components/Sem.jsx
@@ -6,19 +6,21 @@ import "../css/Sem.css";
 function Sem(props) {
     const semData = props.semData;
     return (
-        <Droppable droppableId={semData.name}>
-            {(provided, snapshot) => {
-                return (
-                    <div {...provided.droppableProps} ref={provided.innerRef}>
-                        <div className="sem" style={{backgroundColor: snapshot.isDraggingOver ? 'lightblue' : 'white'}}>
-                            <h2 className="sem-name">{semData.name}</h2>
-                            {semData.mods.map((modData, index) => <Mod key={index} index={index} modData={modData} />)}
-                            {provided.placeholder}
+        <div className="sem">
+            <h2 className="sem-name">{semData.name}</h2>
+            <Droppable droppableId={semData.name}>
+                {(provided, snapshot) => {
+                    return (
+                        <div className="sem-droppable" {...provided.droppableProps} ref={provided.innerRef}>
+                            <div style={{backgroundColor: snapshot.isDraggingOver ? 'lightblue' : 'white'}}>
+                                {semData.mods.map((modData, index) => <Mod key={index} index={index} modData={modData} />)}
+                                {provided.placeholder}
+                            </div>
                         </div>
-                    </div>
-                );
-            }}
-        </Droppable>
+                    );
+                }}
+            </Droppable>
+        </div>
     );
 }
 
