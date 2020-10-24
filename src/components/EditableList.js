49b992d31aad467f754627442e795a0a02e03cec
flxffy <aihui@comp.nus.edu.sg>
2020-10-25 00:07:28 +0800

    Adds degree info to settings page

--- src/components/EditableList.js
+++ src/components/EditableList.js
@@ -2,7 +2,6 @@ import React from "react";
 
 import ListGroup from "react-bootstrap/ListGroup";
 import ButtonGroup from "react-bootstrap/ButtonGroup";
-import Dropdown from "react-bootstrap/Dropdown";
 
 import DeleteIcon from "@material-ui/icons/Clear";
 import MoreIcon from "@material-ui/icons/MoreVert";
@@ -10,9 +9,9 @@ import AddIcon from "@material-ui/icons/Add";
 
 import IconButton from "./IconButton";
 
-const EditableList = ({ items, onDelete, onAdd, addLabel, hasMoreOptions, onOpenMoreOptions, listStyle }) => {
+const EditableList = ({ items, onDelete, onAdd, addLabel, hasMoreOptions, onOpenMoreOptions, ...props }) => {
   return (
-    <ListGroup variant="flush" style={listStyle}>
+    <ListGroup variant="flush" {...props}>
       {items.map((item) => (
         <ListGroup.Item
           key={item}
@@ -38,13 +37,12 @@ const EditableList = ({ items, onDelete, onAdd, addLabel, hasMoreOptions, onOpen
           </ButtonGroup>
         </ListGroup.Item>
       ))}
-      <Dropdown.Divider />
       <ListGroup.Item
         action
         as="button"
         onClick={onAdd}
         className="d-flex justify-content-between align-items-center"
-        style={{ color: "inherit" }}
+        style={{ color: "inherit", borderTop: !items.length && 0 }}
       >
         {addLabel}
         <AddIcon fontSize="small" />
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,23 +1,45 @@
 import React from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
 
 import EditableList from "../components/EditableList";
 
+import "../css/Settings.css";
+
 const Settings = () => {
-  const items = ["item1", "item2", "item3"];
+  const options = {
+    specialisations: {
+      label: "Focus areas/Specialisations",
+      items: ["Software Engineering", "Computer Security"],
+    },
+    majors: { label: "Majors", items: [] },
+    minors: { label: "Minors", items: ["Economics"] },
+  };
 
   const handleDelete = () => console.log("delete clicked");
   const handleAdd = () => console.log("add clicked");
 
   return (
-    <div>
-      <EditableList
-        items={items}
-        onDelete={handleDelete}
-        onAdd={handleAdd}
-        addLabel="Add a new item"
-        listStyle={{ width: "384px" }}
-      />
-    </div>
+    <Container id="root" className="d-flex flex-column justify-content-center align-items-center">
+      <div className="degree-info">
+        <Row className="d-flex justify-content-end">
+          <span className="info-label">Degree program</span>
+          <span className="info-content">BComp in Computer Science (Hons)</span>
+        </Row>
+        {Object.values(options).map(({ label, items }) => (
+          <Row className="d-flex justify-content-end" key={label}>
+            <span className="info-label list-label">{label}</span>
+            <EditableList
+              items={items}
+              onDelete={handleDelete}
+              onAdd={handleAdd}
+              addLabel={`Add ${label.toLowerCase()}`}
+              className="info-content"
+            />
+          </Row>
+        ))}
+      </div>
+    </Container>
   );
 };
 
