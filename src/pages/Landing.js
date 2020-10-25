dbd1272fd5904c97128a04e67e2b7493455bd536
Dominique Ng <dominique@u.nus.edu>
2020-10-25 12:03:21 +0800

    Style landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -2,24 +2,32 @@ import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from "react-bootstrap/Button";
-
 import Typography from "@material-ui/core/Typography";
-import landing from "../img/landing.svg";
 
+import landing from "../img/landing.svg";
+import Btn from "../components/Button";
 import Dashboard from "../components/Dashboard";
 
 const Landing = () => {
   return (
     <Container fluid className="my-2">
       <Row>
-        <Col>
-          <Typography variant="h2" align="left">
-            Supercharge your degree planning with us today
-          </Typography>
-          <Button variant="primary" size="lg">
-            Sign in with SSO
-          </Button>
+        <Col className="ml-4 mt-4">
+          <Container>
+            <Row>
+              <Typography variant="h2" align="left">
+                Supercharge your
+                <br />
+                degree planning
+                <br />
+                with us today
+                <br />
+              </Typography>
+            </Row>
+            <Row className="mt-4">
+              <Btn title="Sign in with SSO" type="primary" />
+            </Row>
+          </Container>
         </Col>
         <Col>
           <img src={landing} className="landing-art" alt="landing-art" />
