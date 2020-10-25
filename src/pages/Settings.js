616d69de778b25795a7055b2924a3321f7c6c1d9
flxffy <aihui@comp.nus.edu.sg>
2020-10-25 11:52:14 +0800

    Adds button group

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,10 +1,11 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 
 import EditableList from "../components/EditableList";
 
-import "../css/Settings.css";
+import "../css/SettingsPage.css";
 
 const Settings = () => {
   const degreeInfo = {
@@ -21,24 +22,30 @@ const Settings = () => {
 
   return (
     <Container id="root" className="d-flex flex-column justify-content-center align-items-center">
-      <div className="degree-info">
-        <Row className="d-flex justify-content-end">
-          <span className="info-label">Degree program</span>
-          <span className="info-content">BComp in Computer Science (Hons)</span>
-        </Row>
-        {Object.values(degreeInfo).map(({ label, items }) => (
-          <Row className="d-flex justify-content-end" key={label}>
-            <span className="info-label list-label">{label}</span>
-            <EditableList
-              items={items}
-              onDelete={handleDelete}
-              onAdd={handleAdd}
-              addLabel={`Add ${label.toLowerCase()}`}
-              className="info-content"
-            />
+      <Row id="row" className="d-flex justify-content-end page-actions">
+        <Button variant="outline-primary">Cancel</Button>
+        <Button variant="outline-primary">Save Changes</Button>
+      </Row>
+      <Row id="row" className="d-flex justify-content-center page-body">
+        <div className="degree-info">
+          <Row className="d-flex justify-content-end">
+            <span className="info-label">Degree program</span>
+            <span className="info-content">BComp in Computer Science (Hons)</span>
           </Row>
-        ))}
-      </div>
+          {Object.values(degreeInfo).map(({ label, items }) => (
+            <Row className="d-flex justify-content-end" key={label}>
+              <span className="info-label list-label">{label}</span>
+              <EditableList
+                items={items}
+                onDelete={handleDelete}
+                onAdd={handleAdd}
+                addLabel={`Add ${label.toLowerCase()}`}
+                className="info-content"
+              />
+            </Row>
+          ))}
+        </div>
+      </Row>
     </Container>
   );
 };
