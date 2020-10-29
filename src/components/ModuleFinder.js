64dd1994c7a40465d98d4f19fdde7e9c3221ed89
flxffy <aihui@comp.nus.edu.sg>
2020-10-29 20:53:30 +0800

    Creates search bar

---
+++ src/components/ModuleFinder.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import _ from "lodash";
+
+import Container from "react-bootstrap/Container";
+import Dropdown from "react-bootstrap/Dropdown";
+
+import SearchBox from "./ClearableSearchBox";
+
+import allModules from "../data/modules-detailed";
+
+const ModuleFinder = ({ moduleToSemMapping }) => {
+  const [modules, setModules] = useState(allModules);
+
+  const handleClearSearch = () => {
+    const textField = document.getElementById("module-search-box");
+    if (!textField.value) return;
+    textField.value = "";
+    setModules(allModules);
+  };
+
+  const debouncedFilter = _.debounce((search) => {
+    const keyword = search.toLowerCase();
+    setModules(
+      allModules.filter(
+        ({ moduleCode, title }) =>
+          !search || moduleCode.toLowerCase().includes(keyword) || title.toLowerCase().includes(keyword)
+      )
+    );
+  }, 350);
+
+  return (
+    <div>
+      <SearchBox id="module-search-box" handleChange={debouncedFilter} handleClear={handleClearSearch} />
+      <ul>
+        {modules.map((module) => {
+          return <li key={module.moduleCode}>{`${module.moduleCode} ${module.title}`}</li>;
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default ModuleFinder;
