004af7933dce675676582219e84575b301ad7b98
Dominique Ng <dominique@u.nus.edu>
2020-11-04 15:15:22 +0800

    Add legend component for degree progress

---
+++ src/components/Legend.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import StopIcon from "@material-ui/icons/Stop";
+
+import "../scss/Legend.scss";
+
+function Legend(props) {
+  return (
+    <div className="Legend">
+      <StopIcon style={{color: props.colorHex}} />
+      <div className="LegendName">{props.title}</div>
+    </div>
+  );
+}
+
+export default Legend;
