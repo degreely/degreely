017835b589895662d815747eb82993a6df2de4ff
flxffy <aihui@comp.nus.edu.sg>
2020-11-04 23:34:15 +0800

    Reformats template data

--- src/components/TemplateOption/ModuleDisplay.js
+++ src/components/TemplateOption/ModuleDisplay.js
@@ -3,8 +3,6 @@ import Typography from "@material-ui/core/Typography";
 
 import "../../css/TemplateOption.css";
 
-const PALETTE = ["#7400B8", "#6930C3", "#5E60CE", "#5390D9"];
-
 const Plan = ({ plan }) => {
   const rows = getRows(plan);
 
@@ -12,17 +10,13 @@ const Plan = ({ plan }) => {
     <div className="plan">
       {rows.map((row, rIndex) => (
         <div key={`Y${rIndex + 1}`} className="d-flex year">
-          {Object.entries(row).map(([sem, modules]) => (
-            <div key={sem} className="semester">
-              <Typography variant="h6">{sem.includes("ST") ? "Special Term" : sem}</Typography>
+          {Object.entries(row).map(([key, { label, modules }]) => (
+            <div key={key} className="semester">
+              <Typography variant="h6">{label}</Typography>
               <div>
-                {modules.map(({ code, title }, mIndex) => (
-                  <div
-                    key={`${mIndex}-${title}`}
-                    className="module"
-                    style={{ backgroundColor: PALETTE[rIndex] }}
-                  >
-                    <span className="font-weight-medium">{code}</span> <span>{title}</span>
+                {modules.map(({ code, name }, mIndex) => (
+                  <div key={`${mIndex}-${name}`} className={`module gradient-color-${rIndex}`}>
+                    <span className="font-weight-medium">{code}</span> <span>{name}</span>
                   </div>
                 ))}
               </div>
