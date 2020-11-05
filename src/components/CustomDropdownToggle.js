17c43f3b72149b3115641ad3813f6b5d49c1379c
flxffy <aihui@comp.nus.edu.sg>
2020-11-05 22:08:59 +0800

    Adds forwardRef

--- src/components/CustomDropdownToggle.js
+++ src/components/CustomDropdownToggle.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { forwardRef } from "react";
 import Button from "react-bootstrap/Button";
 import "../scss/CustomDropdownToggle.scss";
 
-const CustomDropdownToggle = ({ children, ...props }) => {
+const CustomDropdownToggle = forwardRef(({ children, ...props }, ref) => {
   return (
-    <Button id="custom-toggle" {...props}>
+    <Button id="custom-toggle" {...props} ref={ref}>
       {children}
     </Button>
   );
-};
+});
 
 export default CustomDropdownToggle;
