0c556926cdc1e08cf9a008fadc4f5ba530e816f7
Kenneth Fung <fungchenyu@rocketmail.com>
2020-11-06 15:02:23 +0800

    Fix redux actions not editing/adding plan correctly

--- src/redux/actions.js
+++ src/redux/actions.js
@@ -3,9 +3,9 @@ import { createActions } from "reduxsauce";
 export const { Types, Creators: Actions } = createActions(
   {
     changePlan: ["name"],
-    addPlan: ["plan"],
-    renamePlan: ["prevName, newName"],
-    editPlan: ["plan"],
+    addPlan: ["name", "plan"],
+    renamePlan: ["prevName", "newName"],
+    editPlan: ["name", "plan"],
     deletePlan: ["name"],
   },
   {}
