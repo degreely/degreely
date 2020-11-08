1213495e8e3622114faca63f5ddaccc5e2e8312d
flxffy <aihui@comp.nus.edu.sg>
2020-11-08 22:08:25 +0800

    Adds title

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -6,7 +6,9 @@ import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import ArrowBackIcon from "@material-ui/icons/ArrowBackIos";
+import Typography from "@material-ui/core/Typography";
+import HomeIcon from "@material-ui/icons/HomeRounded";
+import EditIcon from "@material-ui/icons/EditOutlined";
 
 import ReadOnlyList from "../components/ReadOnlyList";
 import EditableList from "../components/EditableList";
@@ -75,24 +77,49 @@ const Settings = ({ currentPlanName, plan, handleEditPlan }) => {
                 to="dashboard"
                 variant="outline-primary"
                 className="d-flex align-items-center"
-                style={{ border: 0, fontSize: "1rem" }}
+                style={{ border: 0 }}
               >
-                <ArrowBackIcon fontSize="inherit" style={{ marginRight: "1rem" }} />
+                <HomeIcon style={{ marginRight: "0.5rem" }} />
                 Dashboard
               </Button>
-              <Button variant="outline-primary" onClick={handleStartEditMode}>
-                Edit
-              </Button>
+              <div className="d-flex align-items-center">
+                <Typography variant="h4">Settings</Typography>
+                <Button
+                  onClick={handleStartEditMode}
+                  variant="outline-primary"
+                  className="d-flex align-items-center justify-content-center"
+                  style={{
+                    border: 0,
+                    borderRadius: "50%",
+                    height: "2rem",
+                    width: "2rem",
+                    padding: "0",
+                    marginLeft: "1rem",
+                  }}
+                >
+                  <EditIcon color="inherit" fontSize="small" />
+                </Button>
+              </div>
+              <div />
             </Row>
           ) : (
-            <Row id="row" className="d-flex justify-content-end page-actions">
-              <Button variant="outline-primary" onClick={handleCancel}>
-                Cancel
-              </Button>
-              <Button variant="outline-primary" onClick={handleSaveChanges}>
-                Save Changes
-              </Button>
-            </Row>
+            <>
+              <Row id="row" className="d-flex justify-content-center">
+                <Typography variant="h4">Settings</Typography>
+              </Row>
+              <Row id="row" className="d-flex justify-content-end" style={{ marginBottom: "1.5rem" }}>
+                <Button variant="outline-primary" onClick={handleCancel} style={{ marginRight: "1.5rem" }}>
+                  Cancel
+                </Button>
+                <Button
+                  variant="outline-primary"
+                  onClick={handleSaveChanges}
+                  style={{ marginRight: "2.5rem" }}
+                >
+                  Save Changes
+                </Button>
+              </Row>
+            </>
           )}
 
           <Row id="row" className="d-flex justify-content-center">
