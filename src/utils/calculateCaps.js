71fa134af53835ad97a92897c43d10698e8670e0
Kenneth Fung <fungchenyu@rocketmail.com>
2020-11-09 14:55:36 +0800

    Add functions to calculate projected/actual CAP

---
+++ src/utils/calculateCaps.js
@@ -0,0 +1,55 @@
+export const gradeToCap = (grade) => {
+    switch (grade) {
+        case "A+":
+        case "A":
+            return 5.0;
+        case "A-":
+            return 4.5;
+        case "B+":
+            return 4.0;
+        case "B":
+            return 3.5;
+        case "B-":
+            return 3.0;
+        case "C+":
+            return 2.5;
+        case "C":
+            return 2.0;
+        case "D+":
+            return 1.5;
+        case "D+":
+            return 1.0;
+        case "F":
+            return 0.0;
+        default:
+            return "THIS AIN'T A GRADE!";
+    }
+};
+
+export const calculateProjectedCap = (plan) => {
+    let numerator = 0; // sum(mod CAP * mod MCs)
+    let denominator = 0; // sum(mod MCs)
+    for (const sem of Object.values(plan.sems)) {
+        for (const mod of sem.mods) {
+            numerator += (mod.taken ? gradeToCap(mod.actualGrade) : gradeToCap(mod.projectedGrade)) * mod.mcs;
+            denominator += mod.mcs;
+        }
+    }
+
+    return denominator === 0 ? denominator : numerator / denominator;
+};
+
+export const calculateActualCap = (plan) => {
+    let numerator = 0; // sum(mod CAP * mod MCs)
+    let denominator = 0; // sum(mod MCs)
+    for (const sem of Object.values(plan.sems)) {
+        for (const mod of sem.mods) {
+            // only taken mods used in calculation
+            if (!mod.taken) continue;
+            numerator += gradeToCap(mod.actualGrade) * mod.mcs;
+            denominator += mod.mcs;
+        }
+    }
+
+    return denominator === 0 ? denominator : numerator / denominator;
+};
