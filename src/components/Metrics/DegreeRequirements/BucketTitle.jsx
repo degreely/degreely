9425eb19caafc5d4ca4f164ffa0d15cdf4f70163
flxffy <aihui@comp.nus.edu.sg>
2020-11-11 06:06:22 +0800

    Fixes metrics accordian icon alignment

--- src/components/Metrics/DegreeRequirements/BucketTitle.jsx
+++ src/components/Metrics/DegreeRequirements/BucketTitle.jsx
@@ -1,26 +1,35 @@
 import React from "react";
-import Badge from "react-bootstrap/Badge";
+import ErrorIcon from "@material-ui/icons/Error";
 import ExpandMore from "@material-ui/icons/ExpandMore";
 import ExpandLess from "@material-ui/icons/ExpandLess";
 
 import "../../../scss/Metrics.scss";
 
-function BucketTitle({title, fulfilled, expanded}) {
-    return (
-        <span className="bucket-title">
-            <span className={`bucket-title-text ${fulfilled ? "bucket-title-text-fulfilled" : "bucket-title-text-unfulfilled"}`}>
-                {title}
-            </span>
-            <div>
-                {!fulfilled && <Badge pill variant="danger">!</Badge>}
-                <span className="bucket-title-chevron">
-                    {expanded
-                        ? <ExpandLess style={{color: fulfilled ? "gray" : "red"}} />
-                        : <ExpandMore style={{color: fulfilled ? "gray" : "red"}} />}
-                </span>
-            </div>
+function BucketTitle({ title, fulfilled, expanded }) {
+  return (
+    <span
+      className="bucket-title"
+      style={{ display: "flex", alignItems: "flex-start", justifyContent: "space-between" }}
+    >
+      <span
+        className={`bucket-title-text ${
+          fulfilled ? "bucket-title-text-fulfilled" : "bucket-title-text-unfulfilled"
+        }`}
+      >
+        {title}
+      </span>
+      <div style={{ display: "flex", alignItems: "center", justifyContent: "flex-end", flexWrap: "nowrap" }}>
+        {!fulfilled && <ErrorIcon color="error" />}
+        <span className="bucket-title-chevron">
+          {expanded ? (
+            <ExpandLess style={{ color: fulfilled ? "gray" : "red" }} />
+          ) : (
+            <ExpandMore style={{ color: fulfilled ? "gray" : "red" }} />
+          )}
         </span>
-    );
+      </div>
+    </span>
+  );
 }
 
 export default BucketTitle;
