f69eb12d37bfb6cf7d4c973195168ac2c4de1905
flxffy <aihui@comp.nus.edu.sg>
2020-11-11 02:52:37 +0800

    Implements specific template names

--- src/utils/generatePlanName.js
+++ src/utils/generatePlanName.js
@@ -1,14 +1,14 @@
 const DEFAULT_NAME = "Untitled";
 
-export const generatePlanName = (existingPlans) => {
-  if (!existingPlans[DEFAULT_NAME]) {
-    return DEFAULT_NAME;
+export const generatePlanName = (name = DEFAULT_NAME, existingPlans) => {
+  if (!existingPlans[name]) {
+    return name;
   }
 
   let i = 1;
-  while (existingPlans[DEFAULT_NAME + " " + i]) {
+  while (existingPlans[name + " " + i]) {
     i += 1;
   }
 
-  return DEFAULT_NAME + " " + i;
+  return name + " " + i;
 };
