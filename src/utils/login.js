663b317fc7e16a4bce8f4c200248178a3893d256
flxffy <aihui@comp.nus.edu.sg>
2020-11-11 04:54:36 +0800

    Handles first login

--- src/utils/login.js
+++ src/utils/login.js
@@ -1,6 +1,11 @@
 const login = (history) => {
-  localStorage.setItem("token", "this is a token!");
-  history.push("dashboard");
+  if (localStorage.getItem("token")) {
+    history.push("dashboard");
+  } else {
+    // first login
+    localStorage.setItem("token", "this is a token!");
+    history.push("create-plan");
+  }
 };
 
 export { login };
