b23301374ce6bce59e44c12698d6bcce641515e6
Kenneth Fung <fungchenyu@rocketmail.com>
2020-11-11 08:47:10 +0800

    Fix misaligned template options

--- src/components/TemplateOption/ModuleDisplay.js
+++ src/components/TemplateOption/ModuleDisplay.js
@@ -6,24 +6,34 @@ import "../../css/TemplateOption.css";
 const Plan = ({ sems }) => {
   const rows = getRows(sems);
 
+  let yearColor = 0;
+  let gradientIncreasing = false;
+  const advanceModColorGradient = () => {
+    // warning: yearColor is hard-coded to range between 0 and 4
+    if (yearColor === 0 || yearColor === 4) gradientIncreasing = !gradientIncreasing;
+    yearColor += gradientIncreasing ? 1 : -1;
+  };
   return (
     <div className="sems">
-      {rows.map((row, rIndex) => (
-        <div key={`Y${rIndex + 1}`} className="d-flex year">
-          {Object.entries(row).map(([key, { label, modules }]) => (
-            <div key={key} className="semester">
-              <Typography variant="h6">{label}</Typography>
-              <div>
-                {modules.map(({ code, name }, mIndex) => (
-                  <div key={`${mIndex}-${name}`} className={`module gradient-color-${rIndex}`}>
-                    <span className="font-weight-medium">{code}</span> <span>{name}</span>
-                  </div>
-                ))}
+      {rows.map((row, rIndex) => {
+        if (rIndex > 0) advanceModColorGradient();
+        return (
+          <div key={`Y${rIndex + 1}`} className="d-flex year">
+            {Object.entries(row).map(([key, { label, modules }]) => (
+              <div key={key} className="semester">
+                <Typography variant="h6">{label}</Typography>
+                <div>
+                  {modules.map(({ code, name }, mIndex) => (
+                    <div key={`${mIndex}-${name}`} className={`module gradient-color-${yearColor}`}>
+                      <span className="font-weight-medium">{code}</span> <span>{name}</span>
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      ))}
+            ))}
+          </div>
+        );
+      })}
     </div>
   );
 };
@@ -39,7 +49,16 @@ const getRows = (sems) => {
     if (sem.includes(prev)) {
       row[sem] = modules;
     } else {
-      rows.push(row);
+      // break up row such that we have a max of 2 sems per row
+      const toAdd = Object.entries(row);
+      for (let i = 0; i < toAdd.length; i += 2) {
+        if (i + 1 < toAdd.length) {
+          rows.push({[toAdd[i][0]]: toAdd[i][1], [toAdd[i + 1][0]]: toAdd[i + 1][1]});
+        } else {
+          rows.push({[toAdd[i][0]]: toAdd[i][1]});
+        }
+      }
+
       row = { [sem]: modules };
       prev = sem.substring(0, 2);
     }
